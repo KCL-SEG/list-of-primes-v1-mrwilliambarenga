54cebb90f5330bfd2fcf36e54e94a9f2bf59d809
mrwilliambarenga <williambarenga@gmail.com>
2022-10-03 09:26:39 +0100

    Complete Task

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    
+    list = [2]
+    number = 3
+    count = 1
+
+    while count < number_of_primes:
+        
+        for x in list:
+
+            if number%x == 0:
+                break
+        else:
+            list.append(number)
+            count += 1
+
+        number += 1
+
+
     return list
